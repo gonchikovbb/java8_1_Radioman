664c356fee15901c4b9c5fa9160e309fcbcd147e
Bolot Gonchikov <gonchikovbb@gmail.com>
2022-03-21 21:36:43 +0300

    Radioman_v1

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -3,24 +3,51 @@ package ru.netology;
 public class Radio {
 
     private int currentVolume;
-    //private int currentListening;
+    private int currentStation;
 
     public int getCurrentVolume() {
+
         return currentVolume;
     }
 
-    //public int getCurrentListening() {
-    //    return currentListening;
-    //}
+    public int getCurrentStation() {
+
+        return currentStation;
+    }
 
     public void increaseVolume() {
+        if (currentVolume == 10) {
+            currentVolume = 10;
+        }
         if (currentVolume < 10) {
             currentVolume = currentVolume + 1;
         }
     }
+
     public void reduceVolume() {
+        if (currentVolume == 0) {
+            currentVolume = 0;
+        }
         if (currentVolume > 0) {
             currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void increaseStation() {
+        if (currentStation < 10) {
+            currentStation = currentStation + 1;
+        }
+        if (currentStation == 9) {
+            currentStation = 0;
+        }
+    }
+
+    public void reduceStation() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        }
+        if (currentStation == 0) {
+            currentStation = 9;
         }
     }
 }
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -1,16 +1,51 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
     @org.junit.jupiter.api.Test
-    void increaseVolume() {
+    void shouldIncreaseVolume() {
         Radio rad = new Radio();
         rad.increaseVolume();
 
+        int expected = 1;
+        int actual = rad.getCurrentVolume();
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldReduceVolume() {
+        Radio rad = new Radio();
+        rad.reduceVolume();
+
         int expected = 0;
         int actual = rad.getCurrentVolume();
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldIncreaseStation() {
+        Radio rad = new Radio();
+        rad.increaseStation();
+
+        int expected = 1;
+        int actual = rad.getCurrentStation();
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void shouldReduceStation() {
+        Radio rad = new Radio();
+        rad.reduceStation();
+
+        int expected = 9;
+        int actual = rad.getCurrentStation();
 
         assertEquals(expected,actual);
     }
